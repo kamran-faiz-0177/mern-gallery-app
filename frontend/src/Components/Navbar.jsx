dc53a9a7b326caa9c8c7cb23a21e6ecefab04a88
kamran <muhammad.kamran.cs@gmail.com>
2025-09-19 12:22:35 +0500

    completed the navbar section

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <div className='w-full h-20 bg-black flex items-center justify-center text-white'>
+            <div className='flex gap-4 text-xl'>
+                <Link to="/upload">Upload</Link>
+                <Link to="/gallery">Gallery</Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
